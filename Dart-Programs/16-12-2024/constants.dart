aad7b809ddfab0db4f90ee535d79df74878094fe
Kunal Shah <kunalmanishshah@gmail.com>
2025-01-02 12:32:09 +0530

    Delete Dart-Programs/16-12-2024 directory

--- Dart-Programs/16-12-2024/constants.dart
+++
@@ -1,5 +0,0 @@
-void main(){
-	const int num = 30;
-	print(num);
-	// num = 40;    // Gives error as constants cannot be modified
-}
--- Dart-Programs/16-12-2024/for-loop.dart
+++
@@ -1,9 +0,0 @@
-// Display numbers from 0 to 30 with increment of 4 and skipping numbers 16 and 24.
-void main() {
-  for (int i = 0; i < 31; i += 4) {
-    if (i == 16 || i == 24) {
-      continue;
-    }
-    print(i);
-  }
-}

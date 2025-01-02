b0417db9ad44bb9f645cc28cde4affb57affd694
Kunal Shah <kunalmanishshah@gmail.com>
2025-01-02 12:32:24 +0530

    Delete Dart-Programs/18-12-2024 directory

--- Dart-Programs/18-12-2024/cube-with-while.dart
+++
@@ -1,9 +0,0 @@
-// Create a do-while loop to display the cubes of the next 10 numbers starting  from 30.
-
-void main() {
-  int i = 30;
-  do {
-    print(i * i * i);
-    i++;
-  } while (i < 40);
-}
--- Dart-Programs/18-12-2024/divisible-by-19.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  for (int i = 1; i <= 100; i++) {
-    if (i % 19 == 0) {
-      print(i);
-    }
-  }
-}

65fdad5d669f8acccb6a6716c9d2771b2444b451
Kunal Shah <kunalmanishshah@gmail.com>
2025-01-02 12:36:32 +0530

    Update nested-conditions.dart

--- Dart-Programs/Practical-Codes/02-01-2025/nested-conditions.dart
+++ Dart-Programs/Practical-Codes/02-01-2025/nested-conditions.dart
@@ -7,5 +7,9 @@ void main(){
       print("b is greatest $b");
   }else if (c > a && c > b){
       print("c is greatest $c");
+  }else if (a == b && a == c){
+      print("All three vars are equal");
+  }else if(a == b || a == c || b == c){
+      print("Two vars are equal");
   }
 }
